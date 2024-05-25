02042d8153d65bf0c3f4941f7db742d66abdddbc
amin <amin.samadzadeh@gmail.com>
2024-05-25 11:58:32 +0330

    change something

--- app/main.py
+++ app/main.py
@@ -2,26 +2,38 @@ from flask import Flask, jsonify, request
 from app.env import *
 from app.redisConnection import *
 from app.rateLimiter.RateLimiter import *
+from app.rateLimiter2.RateLimiter import RateLimiter as RateLimiter2
 
 if not redisConnection.exists('requests'):
 	redisConnection.bf().create('requests', 0.01, 1000)
 
 app = Flask(__name__)
 
+# @app.before_request
+# def before():
+# 	print('before_request')
+# 	rt = RateLimiter()
+
+# 	if rt.isDuplicated():
+# 		return jsonify({'status': False}), 400
+
+# 	rt.addToRequests()
+
+# 	if rt.isLimit():
+# 		return jsonify({'status': False}), 429
+
+# 	rt.increment()
+
 @app.before_request
-def before():
+def before2():
 	print('before_request')
-	rt = RateLimiter()
+	rt = RateLimiter2()
 
-	if rt.isDuplicated():
-		return jsonify({'status': False}), 400
+	rt.execute()
 
-	rt.addToRequests()
-
-	if rt.isLimit():
-		return jsonify({'status': False}), 429
-
-	rt.increment()
+	limit, statusCode = rt.isLimit()
+	if limit:
+		return jsonify({'status': False}), statusCode
 
 @app.post('/sample')
 def sample():
